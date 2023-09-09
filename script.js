2f496e4121fdc1d2b7407ed8f27d88e5c016852b
Hafizzargar <96607751+Hafizzargar@users.noreply.github.com>
2023-09-09 14:01:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
-function fibonacci(num) {
+function fibonacci(c) {
 // your code here
+	let c=1;
+let f1=0;
+let f2=1;
+let sum=0;
+if(c==1){
+	return f1;
+}
+if(c==2){
+	return f2;
+}
+let c1=3;
+while(c1<=5){
+	sum=f1+f2;
+	f1=f2;
+	
+	f2=sum;
+	c1++;
+	console.log(sum);
+}
+
 }
 
 module.exports = fibonacci;
