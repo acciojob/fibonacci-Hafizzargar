54d7d2ea866e4dad3d6dc683592fb0012326b264
Hafizzargar <96607751+Hafizzargar@users.noreply.github.com>
2024-02-07 16:59:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -21,13 +21,27 @@ function fibonacci(c) {
 
             // }
             // console.log(sum);
-	if (c <= 1) {
+	// if (c <= 1) {
+ //        return 0;
+ //    } else if (c === 2) {
+ //        return 1;
+ //    } else {
+ //        return fibonacci(c - 1) + fibonacci(c- 2);
+ //    }
+	 if (c === 0) {
         return 0;
-    } else if (c === 2) {
-        return 1;
-    } else {
-        return fibonacci(c - 1) + fibonacci(c- 2);
     }
+
+    let prev = 0;
+    let current = 1;
+
+    for (let i = 2; i <= c; i++) {
+        let next = prev + current;
+        prev = current;
+        current = next;
+    }
+
+    return prev;
 	
 
 }
